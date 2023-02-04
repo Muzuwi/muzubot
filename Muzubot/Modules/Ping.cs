a6b3c2935179ef7bcf766cfaa01704bd8b91288e
Muzuwi <kedziora99@gmail.com>
2023-02-04 22:46:39 +0100

    module: Add ping command

---
+++ Muzubot/Modules/Ping.cs
@@ -0,0 +1,17 @@
+using System.Diagnostics;
+using Muzubot.Commands;
+
+namespace Muzubot.Modules;
+
+public class Ping
+{
+    [CommandOpts("ping")]
+    public async Task EnterDungeon(CommandContext context)
+    {
+        var tmiTime = DateTimeOffset.FromUnixTimeMilliseconds(long.Parse(context.Meta.TmiSentTs)).LocalDateTime;
+        var uptime = DateTime.Now - Process.GetCurrentProcess().StartTime;
+        var latency = DateTime.Now - tmiTime;
+        context.Reply(
+            $"Pong! Uptime: {uptime:dd}d{uptime:hh}h{uptime:mm}m, latency to TMI: {latency.TotalMilliseconds}ms");
+    }
+}
