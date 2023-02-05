782ddfeb85dee120e6a2f9b1846f911c39ed51a6
Muzuwi <kedziora99@gmail.com>
2023-02-05 10:50:21 +0100

    command: Test storage API from command

--- Muzubot/Modules/MyNewCommand.cs
+++ Muzubot/Modules/MyNewCommand.cs
@@ -1,21 +1,37 @@
 using Microsoft.Extensions.Logging;
 using Muzubot.Commands;
+using Muzubot.Storage;
 
 namespace Muzubot.Modules;
 
 public class MyNewCommand
 {
-    public MyNewCommand(ILogger<MyNewCommand> logger)
+    public MyNewCommand(ILogger<MyNewCommand> logger, IStorageConnector connector)
     {
         _logger = logger;
+        _connector = connector;
     }
 
     [CommandOpts("ed")]
     public async Task EnterDungeon(CommandContext context)
     {
-        _logger.LogInformation("Welcome from command module");
-        context.Reply($"It is now {DateTime.Now}");
+        var uid = context.Meta.UserId;
+        var userData = await _connector.FetchOrCreateUserData(uid);
+        if (userData is null)
+        {
+            _logger.LogError($"Failed fetching user data for user {uid}");
+            return;
+        }
+
+        var random = new Random();
+        var xp = random.NextInt64(0, 2048);
+        userData.Experience += xp;
+        await _connector.CommitUserData(userData);
+
+        context.Reply(
+            $"@{context.Meta.Username} | You have gained {xp} experience! You now have {userData.Experience} XP.");
     }
 
     private readonly ILogger<MyNewCommand> _logger;
+    private readonly IStorageConnector _connector;
 }
