63fdb4b87d85aea26878b6df70dbff068a55cb87
Muzuwi <kedziora99@gmail.com>
2023-02-07 20:59:47 +0100

    debug: Add db fetch/update testing to the sample command

--- Muzubot/Modules/MyNewCommand.cs
+++ Muzubot/Modules/MyNewCommand.cs
@@ -1,18 +1,21 @@
 using Microsoft.Extensions.Logging;
 using Muzubot.Commands;
 using Muzubot.Storage;
+using Npgsql;
+using NpgsqlTypes;
 
 namespace Muzubot.Modules;
 
 public class MyNewCommand
 {
-    public MyNewCommand(ILogger<MyNewCommand> logger, IStorageConnector connector)
+    public MyNewCommand(ILogger<MyNewCommand> logger, IStorageConnector connector, NpgsqlDataSource dbSource)
     {
         _logger = logger;
         _connector = connector;
+        _dbSource = dbSource;
     }
 
-    [CommandOpts("ed")]
+    [CommandOpts("ed", 0)]
     public async Task EnterDungeon(CommandContext context)
     {
         var uid = context.Meta.UserId;
@@ -25,13 +28,37 @@ public class MyNewCommand
 
         var random = new Random();
         var xp = random.NextInt64(0, 2048);
-        userData.Experience += xp;
-        await _connector.CommitUserData(userData);
+
+        await using var connection = await _dbSource.OpenConnectionAsync();
+        await using var cmd1 = new NpgsqlCommand(
+            "INSERT INTO Dungeon (UID, Experience)" +
+            "VALUES (($1), ($2))" +
+            "ON CONFLICT (UID) DO " +
+            "UPDATE SET Experience = Dungeon.Experience + excluded.Experience", connection
+        )
+        {
+            Parameters =
+            {
+                new() { Value = uid },
+                new() { Value = xp }
+            }
+        };
+        var rows = await cmd1.ExecuteNonQueryAsync();
+        _logger.LogDebug("{:0} rows affected", rows);
+
+        await using var cmd2 = new NpgsqlCommand("SELECT Experience FROM Dungeon WHERE UID = ($1)", connection)
+        {
+            Parameters = { new() { Value = uid } }
+        };
+        await using var reader = await cmd2.ExecuteReaderAsync();
+        await reader.ReadAsync();
+        var newExp = reader.GetInt32(0);
 
         context.Reply(
-            $"@{context.Meta.Username} | You have gained {xp} experience! You now have {userData.Experience} XP.");
+            $"@{context.Meta.Username} | You have gained {xp} experience! You now have {newExp} XP.");
     }
 
     private readonly ILogger<MyNewCommand> _logger;
     private readonly IStorageConnector _connector;
+    private readonly NpgsqlDataSource _dbSource;
 }
