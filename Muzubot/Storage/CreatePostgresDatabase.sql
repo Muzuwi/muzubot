f53b7531e9a528f6d5b275dcffccd112c582ac48
Muzuwi <kedziora99@gmail.com>
2023-02-11 11:37:55 +0100

    docs: Add database creation script

---
+++ Muzubot/Storage/CreatePostgresDatabase.sql
@@ -0,0 +1,33 @@
+create table dungeon
+(
+    uid        varchar(255) PRIMARY KEY,
+    experience int
+);
+
+create table command_usage
+(
+    uid       varchar(255)             NOT NULL,
+    command   varchar(255)             NOT NULL,
+    last_used timestamp with time zone NULL,
+    PRIMARY KEY (uid, command)
+);
+
+create table banned
+(
+    uid    varchar(255) PRIMARY KEY,
+    reason varchar(255)             NOT NULL,
+    expiry timestamp with time zone NULL
+);
+
+create table suggestions
+(
+    identifier SERIAL PRIMARY KEY,
+    author_uid varchar(255) NOT NULL,
+    "category" varchar(32)  NOT NULL,
+    contents   varchar(256) NOT NULL
+);
+
+create table statistics
+(
+    commands_processed int
+);
